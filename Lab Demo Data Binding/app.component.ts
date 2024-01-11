75a92100bee3aa5f02e3faed02c765466b7fcd90
Louise <114331690+BlankuZero@users.noreply.github.com>
2024-01-11 15:43:52 +0800

    Update app.component.ts

--- Lab Demo Data Binding/app.component.ts
+++ Lab Demo Data Binding/app.component.ts
@@ -1,1 +1,18 @@
+export class AppComponent {
+  title = 'This string is interpolated';
+  clientName = 'Adrian';
+  appliedCSS = 'green';
+  notAppliedCSS = false;
+  myColor = 'red';  
+  
 
+  clickCount = 0
+    clickMe() {
+      this.clickCount++;
+    }
+  
+    resetClickCount() {
+      this.clickCount = 0;
+    }
+
+}
